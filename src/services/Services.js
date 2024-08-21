273583dab061a06aa140c55f54e35ce97e1323c7
MchevezS <168207997+MchevezS@users.noreply.github.com>
2024-08-21 15:46:46 -0600

    Metodo GetUsers

--- src/services/Services.js
+++ src/services/Services.js
@@ -19,6 +19,30 @@ async function postUsers(userData) {
 
 export{postUsers}
 
+//Metodo Get: Obtiene informacio
+async function getUsers() {
+    try {
+        const response = await fetch('http://localhost:3001/users', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        if (!response.ok) {
+            throw new Error('Error fetching users');
+        }
+
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error fetching users:', error);
+        throw error;
+    }
+}
+
+export { getUsers};
+
 
 
 
